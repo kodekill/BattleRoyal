129b35a5a98ab1728f6ce1b423e8aca0c50d54ac
kodekill <brian.falor@gmail.com>
2018-03-27 22:21:21 -0600

    added better boat moving capabilities

--- js/player.js
+++ js/player.js
@@ -43,22 +43,24 @@ function makeBoat(){
         if(elapsedTime > 1){
             //console.log(elapsedTime);
             //Controls for boat movement
-            if(rightKey && this.x < canvas.width){
+            if(rightKey){
                 this.rotate += (this.speed / 75000) * elapsedTime;
                 //console.log(this.rotate);
             }
-            else if(leftKey && this.x > 0){
+            else if(leftKey){
                 this.rotate -= (this.speed / 75000) * elapsedTime;
                 //console.log(this.rotate);
             }
 
 
             else if(upKey && this.y > 0){
-                this.y -= (this.speed / 1000) * elapsedTime;
-                //console.log((this.speed / 100) * elapsedTime);
+                this.x += dx * (this.speed / 1000) * elapsedTime;
+                this.y -= dy * (this.speed / 1000) * elapsedTime;
+                //console.log(this.x);
             }
             else if(downKey && this.y > 0){
-                this.y += (this.speed / 1000) * elapsedTime;
+                this.x -= dx * (this.speed / 1000) * elapsedTime;
+                this.y += dy * (this.speed / 1000) * elapsedTime;
                 //console.log(this.speed);
             }
         }
@@ -72,13 +74,18 @@ function makeBoat(){
 function getPicture(rotate){
     var Circumference = 2*Math.PI;
 
+
+    var hi = rotate * 180 / Math.PI;
+
+    //console.log(hi);
+
     if(rotate > (Circumference)){
         rotate = Math.abs(rotate % Circumference);
     }
     else if(rotate < Circumference){
         rotate = Math.abs(rotate % Circumference);
     }
-    console.log(rotate);
+    //console.log(rotate);
     if(rotate == 0){
         image = BS_E;
     }
@@ -86,34 +93,50 @@ function getPicture(rotate){
     else{
         if(rotate > ((11*Math.PI)/6) && rotate < Math.PI/6){
             image = BS_E;
+            this.dy = 0;
+            this.dx = 1; 
         }
 
         else if(rotate > Math.PI/6 && rotate < Math.PI/3){
             image = BS_NE;
+            this.dy = 1;
+            this.dx = 1;
         }
 
         else if(rotate > Math.PI/3 && rotate < ((2*Math.PI)/3)){
             image = BS_N;
+            this.dy = 1;
+            this.dx = 0;
         }
 
         else if(rotate > ((2*Math.PI)/3) && rotate < ((5*Math.PI)/6)){
             image = BS_NW;
+            this.dy = 1;
+            this.dx = -1;
         }
 
         else if(rotate > ((5*Math.PI)/6) && rotate < ((7*Math.PI)/6)){
             image = BS_W;
+            this.dy = 0;
+            this.dx = -1;
         }
 
         else if(rotate > ((7*Math.PI)/6) && rotate < ((4*Math.PI)/3)){
             image = BS_SW;
+            this.dy = -1;
+            this.dx = -1;
         }
 
         else if(rotate > ((4*Math.PI)/3) && rotate < ((5*Math.PI)/3)){
             image = BS_S;
+            this.dy = -1;
+            this.dx = 0;
         }
 
         else if(rotate > ((5*Math.PI)/3) && rotate < ((11*Math.PI)/6)){
             image = BS_SE;
+            this.dy = -1;
+            this.dx = 1;
         }
     }
     

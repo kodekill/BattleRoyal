c3929ce8c5424333a975f271fee0bed783541616
kodekill <brian.falor@gmail.com>
2018-03-28 22:29:16 -0600

    Fixed the rotating animation and added map sprites.

--- js/player.js
+++ js/player.js
@@ -11,9 +11,10 @@ function makeBoat(){
 	this.y = 525;
 	this.dx = 0;
     this.dy = 0;
-    this.rotate = 0; 
+    this.rotate = 0;
     this.speed = medium;
     this.status = 1;
+    this.image = BS_E;
     
     this.drawBoat = function(){
 		var canvas = document.getElementById("myCanvas");
@@ -21,18 +22,8 @@ function makeBoat(){
 
         if(this.status == 1){
             //console.log(this.rotate);
-
-            image = getPicture(this.rotate);
-            //console.log(image);
-            //centerX = this.x-(this.image.width/2);
-            //centerY = this.y-(this.image.height/2);
-            
-            ctx.save();
-                ctx.translate(this.x, this.y);
-                //ctx.rotate(this.rotate);
-                ctx.translate(-this.x, -this.y);
-                ctx.drawImage(image, (this.x - image.width/2), (this.y - image.height/2), image.width, image.height);
-            ctx.restore();
+            this.image = getDirection(this.rotate);
+            ctx.drawImage(this.image, (this.x - image.width/2), (this.y - image.height/2), image.width, image.height);
         }
     };
 
@@ -41,14 +32,13 @@ function makeBoat(){
         var ctx = canvas.getContext("2d");
 
         if(elapsedTime > 1){
-            //console.log(elapsedTime);
             //Controls for boat movement
             if(rightKey){
-                this.rotate += (this.speed / 75000) * elapsedTime;
+                this.rotate -= (this.speed / 75000) * elapsedTime;
                 //console.log(this.rotate);
             }
             else if(leftKey){
-                this.rotate -= (this.speed / 75000) * elapsedTime;
+                this.rotate += (this.speed / 75000) * elapsedTime;
                 //console.log(this.rotate);
             }
 
@@ -59,45 +49,34 @@ function makeBoat(){
                 //console.log(this.x);
             }
             else if(downKey && this.y > 0){
-                this.x -= dx * (this.speed / 1000) * elapsedTime;
-                this.y += dy * (this.speed / 1000) * elapsedTime;
+                this.x -= dx * (this.speed / 2000) * elapsedTime;
+                this.y += dy * (this.speed / 2000) * elapsedTime;
                 //console.log(this.speed);
             }
         }
-
-        //this.x += (this.dx*this.speed);
-        //this.y += (this.dy*this.speed);
     };
 };
 
 
-function getPicture(rotate){
+function getDirection(rotate){
     var Circumference = 2*Math.PI;
 
-
-    var hi = rotate * 180 / Math.PI;
-
-    //console.log(hi);
-
-    if(rotate > (Circumference)){
+    if(rotate > 0){
         rotate = Math.abs(rotate % Circumference);
     }
     else if(rotate < Circumference){
-        rotate = Math.abs(rotate % Circumference);
+        rotate = Circumference - (Math.abs(rotate % Circumference));
     }
-    //console.log(rotate);
-    if(rotate == 0){
-        image = BS_E;
-    }
+    console.log(rotate);
 
-    else{
-        if(rotate > ((11*Math.PI)/6) && rotate < Math.PI/6){
+ 
+        if(rotate > ((23*Math.PI)/12) || rotate < Math.PI/12){
             image = BS_E;
             this.dy = 0;
             this.dx = 1; 
         }
 
-        else if(rotate > Math.PI/6 && rotate < Math.PI/3){
+        else if(rotate > Math.PI/12 && rotate < Math.PI/3){
             image = BS_NE;
             this.dy = 1;
             this.dx = 1;
@@ -109,19 +88,19 @@ function getPicture(rotate){
             this.dx = 0;
         }
 
-        else if(rotate > ((2*Math.PI)/3) && rotate < ((5*Math.PI)/6)){
+        else if(rotate > ((2*Math.PI)/3) && rotate < ((11*Math.PI)/12)){
             image = BS_NW;
             this.dy = 1;
             this.dx = -1;
         }
 
-        else if(rotate > ((5*Math.PI)/6) && rotate < ((7*Math.PI)/6)){
+        else if(rotate > ((11*Math.PI)/12) && rotate < ((13*Math.PI)/12)){
             image = BS_W;
             this.dy = 0;
             this.dx = -1;
         }
 
-        else if(rotate > ((7*Math.PI)/6) && rotate < ((4*Math.PI)/3)){
+        else if(rotate > ((13*Math.PI)/12) && rotate < ((4*Math.PI)/3)){
             image = BS_SW;
             this.dy = -1;
             this.dx = -1;
@@ -133,13 +112,11 @@ function getPicture(rotate){
             this.dx = 0;
         }
 
-        else if(rotate > ((5*Math.PI)/3) && rotate < ((11*Math.PI)/6)){
+        else if(rotate > ((5*Math.PI)/3) && rotate < ((23*Math.PI)/12)){
             image = BS_SE;
             this.dy = -1;
             this.dx = 1;
         }
-    }
     
-    //console.log(image);
     return image;
 }
---
+++ sprites/Summer Tiles.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Summer Tiles" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="342" columns="18">
+ <image source="Summer Tiles.png" width="626" height="659"/>
+ <terraintypes>
+  <terrain name="New Terrain" tile="189"/>
+ </terraintypes>
+ <tile id="173" terrain="0,0,0,0"/>
+ <tile id="177" terrain="0,0,0,0"/>
+ <tile id="188" terrain="0,0,0,0"/>
+ <tile id="189" terrain="0,0,0,0"/>
+ <tile id="192" terrain="0,0,0,0"/>
+ <tile id="199" terrain=",,0,0"/>
+ <tile id="200" terrain=",,0,0"/>
+ <tile id="201" terrain=",,0,0"/>
+ <tile id="204" terrain=",0,,0"/>
+ <tile id="205" terrain=",0,,0"/>
+ <tile id="206" terrain=",0,,0"/>
+ <tile id="208" terrain=",,,0"/>
+ <tile id="209" terrain=",,,0"/>
+ <tile id="213" terrain="0,,0,"/>
+ <tile id="214" terrain="0,,0,"/>
+ <tile id="215" terrain="0,,0,"/>
+ <tile id="216" terrain=",,0,"/>
+ <tile id="217" terrain="0,0,,"/>
+ <tile id="218" terrain="0,0,,"/>
+ <tile id="219" terrain="0,0,,"/>
+ <tile id="220" terrain=",0,,"/>
+ <tile id="221" terrain=",0,,"/>
+ <tile id="222" terrain="0,,,"/>
+ <tile id="223" terrain="0,,,"/>
+</tileset>
